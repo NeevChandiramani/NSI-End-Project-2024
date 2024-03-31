bebc7ea5bd4c81bb970badebccd904b6bb6d9411
NeevChandiramani <neev.chandiramani@gmail.com>
2024-03-31 20:02:40 +0200

    update

---
+++ chess.py
@@ -0,0 +1,6 @@
+G = {"A" : ["" for i in range(8)],
+"B" : ["" for i in range(8)],
+"C" : ["" for i in range(8)],
+"D" : ["" for i in range(8)],
+"E" G = {"A" : ["" for i in range(8)],
+"B" : ["" for i in range(8)],}
