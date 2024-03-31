3ece9def2d7cc53dab3c2845969e93b8311785e9
NeevChandiramani <neev.chandiramani@gmail.com>
2024-03-31 20:38:48 +0200

    update

--- chess.py
+++ chess.py
@@ -1,14 +1,25 @@
 import pygame
 # import random --> IA with different difficulties
 
-# Print the chessboard.png file with pygame
+# import pygame
 
-screen = pygame.display.set_mode((640,640))
+pygame.init()
+
+# Load the image
 chessboard = pygame.image.load("chessboard.png")
-screen.blit(chessboard,(0,0))
-pygame.display.flip() 
 
+# Set the display mode
+screen = pygame.display.set_mode(chessboard.get_size())
 
+# Update the display
+pygame.display.flip()
+
+# Wait for the user to close the window
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit(0) 
 
 pion1 = ('blanc','cavalier')
 
