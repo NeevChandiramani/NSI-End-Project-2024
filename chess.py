bbfc23fe7dca099ebeccd7bcfea5eef2e41a60aa
NeevChandiramani <neev.chandiramani@gmail.com>
2024-03-31 20:37:12 +0200

    Co-authored-by: Arisen3798 <Arisen3798@users.noreply.github.com>

--- chess.py
+++ chess.py
@@ -1,14 +1,24 @@
 import pygame
 # import random --> IA with different difficulties
 
+# Print the chessboard.png file with pygame
+
+screen = pygame.display.set_mode((640,640))
+chessboard = pygame.image.load("chessboard.png")
+screen.blit(chessboard,(0,0))
+pygame.display.flip() 
+
+
+
 pion1 = ('blanc','cavalier')
 
 prop = {'cavalier' : ('',''), 'pion' : ('','')}
 
 #grid of 8x8 
-def grid():
-    H8 = {}
-    for i in range(8):
-        H8[chr(65)+i] = ["","","","","","","",""]
-    return(H8)
-print(grid())
+#def grid():
+#    H8 = {}
+#    for i in range(8):
+#        H8[ord(65)+i] = ["","","","","","","",""]
+#    return(H8)
+#print(grid())
+
