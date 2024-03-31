ce12b648415832d0c6717da7080d45c1180519e0
NeevChandiramani <neev.chandiramani@gmail.com>
2024-03-31 20:23:36 +0200

    update

--- chess.py
+++ chess.py
@@ -1,4 +1,4 @@
-# import pygame
+import pygame
 # import random --> IA with different difficulties
 
 pion1 = ('blanc','cavalier')
@@ -9,6 +9,6 @@ prop = {'cavalier' : ('',''), 'pion' : ('','')}
 def grid():
     H8 = {}
     for i in range(8):
-        H8.append([0,0,0,0,0,0,0,0])
+        H8[chr(65)+i] = ["","","","","","","",""]
     return(H8)
 print(grid())
