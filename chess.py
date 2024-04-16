7aed9b1d8d879fb2ec56f511896145985e8464f6
NeevChandiramani <neev.chandiramani@gmail.com>
2024-04-16 15:25:57 +0200

    update

--- chess.py
+++ chess.py
@@ -1,6 +1,91 @@
 import pygame
-# import random
+#import random
 #IA with different difficulties
+
+p_b_1 = ("pion","blanc")
+p_b_2 = ("pion","blanc")
+p_b_3 = ("pion","blanc")
+p_b_4 = ("pion","blanc")
+p_b_5 = ("pion","blanc")
+p_b_6 = ("pion","blanc")
+p_b_7 = ("pion","blanc")
+p_b_8 = ("pion","blanc")
+
+t_b_1 = ("tour","blanc")
+t_b_2 = ("tour","blanc")
+
+c_b_1 = ("cavalier","blanc")
+c_b_2 = ("cavalier","blanc")
+
+f_b_1 = ("fou","blanc")
+f_b_2 = ("fou","blanc")
+
+k_b = ("roi","blanc")
+q_b = ("reine","blanc")
+
+
+
+p_n_1 = ("pion","noir")
+p_n_2 = ("pion","noir")
+p_n_3 = ("pion","noir")
+p_n_4 = ("pion","noir")
+p_n_5 = ("pion","noir")
+p_n_6 = ("pion","noir")
+p_n_7 = ("pion","noir")
+p_n_8 = ("pion","noir")
+
+t_n_1 = ("tour","noir")
+t_n_2 = ("tour","noir")
+
+c_n_1 = ("cavalier","noir")
+c_n_2 = ("cavalier","noir")
+
+f_n_1 = ("fou","noir")
+f_n_2 = ("fou","noir")
+
+k_n = ("roi","noir")
+q_n = ("reine","noir")
+
+
+vide = ("","")
+
+
+Echequier = [[t_b_1,c_b_1,f_b_1,q_b,k_b,f_b_2,c_b_2,t_b_2],
+             [p_b_1,p_b_2,p_b_3,p_b_4,p_b_5,p_b_6,p_b_7,p_b_8],
+             [vide,vide,vide,vide,vide,vide,vide,vide],
+             [vide,vide,vide,vide,vide,vide,vide,vide],
+             [vide,vide,vide,vide,vide,vide,vide,vide],
+             [vide,vide,vide,vide,vide,vide,vide,vide],
+             [p_n_1,p_n_2,p_n_3,p_n_4,p_n_5,p_n_6,p_n_7,p_n_8],
+             [t_n_1,c_n_1,f_n_1,q_n,k_n,f_n_2,c_n_2,t_n_2]]
+
+cases_existantes = []
+
+def case(s):
+    global Echequier
+    return Echequier[ord(s[0])-65][int(s[1])-1]
+
+
+print(case("A2"))
+
+def deplas(s):
+    r = [""]
+    colonne = s[0]
+    ligne = s[1]
+    a = case(s)
+    ( tipe , couleur ) = a
+    if tipe == "" :
+        return []
+
+
+
+
+
+
+
+
+
+
 
 pygame.init()
 
--- readme.md
+++ readme.md
@@ -7,7 +7,6 @@
 - [Installation](#installation)
 - [Usage](#usage)
 - [Contributing](#contributing)
-- [License](#license)
 
 ## Installation
 [Provide instructions on how to install and set up your project]
@@ -17,6 +16,3 @@
 
 ## Contributing
 [Explain how others can contribute to your project]
-
-## License
-[Specify the license under which your project is distributed]
